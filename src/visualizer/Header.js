bcd62b46a6f5b5f07acb6ef1a47f9833b2eafacd
Harsh Soni <harsh66287@gmail.com>
2020-11-20 15:30:55 +0530

    Header added

---
+++ src/visualizer/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Visualizer.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="main-header">
+        <div className="container">
+          <h1>Path Finding Visualizer</h1>
+        </div>
+      </div>
+    );
+  }
+}
